2bd3e0fbf9bae0a62630264b666aba6c8e0a08e6
Paul ALexis Herida <paulalexisherida@gmail.com>
2026-05-03 00:55:20 +0800

    Expand CMS: add Navigation, About Page, Contact Page, Footer editing (Phase 1 - Admin UI)

--- api/settings.js
+++ api/settings.js
@@ -7,18 +7,64 @@ const ALLOWED_KEYS = [
   // Site basics
   'site_name', 'site_tagline', 'facebook_page_url',
   'contact_email', 'contact_phone', 'contact_address', 'maintenance_mode',
+  
+  // Navigation & Header
+  'brand_name', 'brand_tagline',
+  'nav_home', 'nav_about', 'nav_news', 'nav_events', 'nav_gallery', 'nav_contact',
+  'nav_cta_text',
+  'topbar_text1', 'topbar_text2',
+  
   // Hero section
   'hero_badge', 'hero_title_line1', 'hero_title_highlight', 'hero_title_line2',
   'hero_description', 'hero_btn1_text', 'hero_btn2_text',
   'hero_card_title', 'hero_card_subtitle',
   'hero_tag1', 'hero_tag2', 'hero_tag3',
+  
   // Stats
   'stat1_value', 'stat1_suffix', 'stat1_label',
   'stat2_value', 'stat2_suffix', 'stat2_label',
   'stat3_value', 'stat3_suffix', 'stat3_label',
   'stat4_value', 'stat4_suffix', 'stat4_label',
+  
   // Mission & Vision
-  'mission_text', 'vision_text', 'core_values_text', 'lgu_commitment_text'
+  'mission_text', 'vision_text', 'core_values_text', 'lgu_commitment_text',
+  
+  // About Page
+  'about_page_title', 'about_page_desc',
+  'about_intro_p1', 'about_intro_p2', 'about_intro_p3',
+  'about_established_year',
+  'about_highlight1', 'about_highlight2', 'about_highlight3', 'about_highlight4',
+  'service1_title', 'service1_desc', 'service1_icon',
+  'service2_title', 'service2_desc', 'service2_icon',
+  'service3_title', 'service3_desc', 'service3_icon',
+  'service4_title', 'service4_desc', 'service4_icon',
+  'service5_title', 'service5_desc', 'service5_icon',
+  'service6_title', 'service6_desc', 'service6_icon',
+  'lgu_involvement_text',
+  
+  // Contact Page
+  'contact_page_title', 'contact_page_desc',
+  'contact_office_hours',
+  'contact_form_name_label', 'contact_form_email_label',
+  'contact_form_subject_label', 'contact_form_message_label',
+  'contact_form_submit_text',
+  
+  // Events Page
+  'events_page_title', 'events_page_desc',
+  'events_empty_text',
+  
+  // Gallery Page
+  'gallery_page_title', 'gallery_page_desc',
+  
+  // News Page
+  'news_page_title', 'news_page_desc',
+  'news_search_placeholder',
+  
+  // Footer
+  'footer_description',
+  'footer_section1_title', 'footer_section2_title', 'footer_section3_title',
+  'footer_copyright',
+  'social_facebook', 'social_email', 'social_phone'
 ];
 
 module.exports = async (req, res) => {
