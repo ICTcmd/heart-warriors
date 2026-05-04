87c27070aec7aad3ca26324e67b20b703adb2c0a
Heart Warriors <heartwarriors@bago.gov.ph>
2026-05-04 14:00:45 +0800

    fix: replace book emoji with official Facebook SVG on floating button

--- assets/js/main.js
+++ assets/js/main.js
@@ -447,7 +447,11 @@ floatingBtns.innerHTML = `
   <a href="https://www.facebook.com/profile.php?id=61589233370202"
      target="_blank" rel="noopener" class="float-btn float-btn-fb"
      aria-label="Visit our Facebook page">
-    <span class="float-icon">📘</span>
+    <span class="float-icon">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="#fff" aria-hidden="true">
+        <path d="M24 12.073C24 5.405 18.627 0 12 0S0 5.405 0 12.073C0 18.1 4.388 23.094 10.125 24v-8.437H7.078v-3.49h3.047V9.41c0-3.025 1.792-4.697 4.533-4.697 1.312 0 2.686.236 2.686.236v2.97h-1.513c-1.491 0-1.956.93-1.956 1.886v2.267h3.328l-.532 3.49h-2.796V24C19.612 23.094 24 18.1 24 12.073z"/>
+      </svg>
+    </span>
     <span class="float-label">Facebook</span>
   </a>
 `;
